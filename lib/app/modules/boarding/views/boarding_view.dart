8c0b19b73ce63fb76f09f3a3583a4245770836ff
Fushigo <rizky23ska@gmail.com>
2024-03-04 19:51:14 +0700

    update boarding

--- lib/app/modules/boarding/views/boarding_view.dart
+++ lib/app/modules/boarding/views/boarding_view.dart
@@ -1,4 +1,5 @@
 import 'package:bacayuk/app/data/constant/global.dart';
+import 'package:bacayuk/app/routes/app_pages.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_onboarding_slider/flutter_onboarding_slider.dart';
 
@@ -20,27 +21,33 @@ class BoardingView extends GetView<BoardingController> {
             centerBackground: true,
             pageBackgroundColor: Colors.white,
             headerBackgroundColor: Colors.white,
-            finishButtonText: "Start now",
+            finishButtonText: "Start Now",
+            onFinish: () => Get.offAllNamed(Routes.HOME),
             finishButtonStyle:
                 FinishButtonStyle(backgroundColor: Colors.blue.shade900),
-            skipTextButton: const Text("Skip"),
+            skipTextButton: Text(
+              "Skip",
+              style: TextStyle(
+                  fontFamily: GlobalVariable.fontPoppins,
+                  fontSize: GlobalVariable.textlg),
+            ),
             background: [
               Image.asset(
                 "asset/image/slider_1.png",
-                width: 430,
-                height: 450,
+                width: 400,
+                height: 420,
                 alignment: Alignment.center,
               ),
               Image.asset(
                 "asset/image/slider_2.png",
-                width: 430,
-                height: 450,
+                width: 400,
+                height: 420,
                 alignment: Alignment.center,
               ),
               Image.asset(
                 "asset/image/slider_3.png",
-                width: 430,
-                height: 450,
+                width: 400,
+                height: 420,
                 alignment: Alignment.center,
               ),
             ],
@@ -86,23 +93,76 @@ class BoardingView extends GetView<BoardingController> {
               ),
               Container(
                 padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: const Column(
+                child: Column(
                   children: <Widget>[
-                    SizedBox(
+                    const SizedBox(
                       height: 480,
                     ),
-                    Text("Description")
+                    RichText(
+                      textAlign: TextAlign.center,
+                      text: TextSpan(
+                          style: TextStyle(
+                              fontFamily: GlobalVariable.fontPoppins,
+                              fontSize: GlobalVariable.heading_2,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.black),
+                          children: const [
+                            TextSpan(text: "Kemudahan dalam melakukan \n"),
+                            TextSpan(
+                                text: "pencarian ",
+                                style: TextStyle(color: Colors.blueAccent)),
+                            TextSpan(
+                              text: "buku perpustakaan",
+                            )
+                          ]),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      "Kami hadir dengan fitur khusus yang membantu proses pencarian buku perpustakaan melalui sistem pencarian. Dengan adanya fitur ini anda dapat mencari berbagai macam jenis kategori koleksi buku dengan cepat dan mudah",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontFamily: GlobalVariable.fontPoppins,
+                          fontSize: GlobalVariable.textmd),
+                    )
                   ],
                 ),
               ),
               Container(
                 padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: const Column(
+                child: Column(
                   children: <Widget>[
-                    SizedBox(
+                    const SizedBox(
                       height: 480,
                     ),
-                    Text("Description")
+                    RichText(
+                      textAlign: TextAlign.center,
+                      text: TextSpan(
+                          style: TextStyle(
+                              fontFamily: GlobalVariable.fontPoppins,
+                              fontSize: GlobalVariable.heading_2,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.black),
+                          children: const [
+                            TextSpan(
+                                text:
+                                    "Baca di Mana Saja, Kapan Saja \n dengan "),
+                            TextSpan(
+                                text: "Baca Yuk!",
+                                style: TextStyle(color: Colors.blueAccent))
+                          ]),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      "Fitur baca online yang memungkinkan Anda menikmati buku tanpa perlu repot meminjam buku fisik di perpustakaan.",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontFamily: GlobalVariable.fontPoppins,
+                          fontSize: GlobalVariable.textmd),
+                    )
                   ],
                 ),
               )
--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -22,7 +22,7 @@ part 'app_routes.dart';
 class AppPages {
   AppPages._();
 
-  static const INITIAL = Routes.REGISTER;
+  static const INITIAL = Routes.SPLASH;
 
   static final routes = [
     GetPage(
